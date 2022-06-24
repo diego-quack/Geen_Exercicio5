6aff23c40863e4621ef25e5f7588775a99377bfd
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-06-24 16:21:40 -0300

    Classe MedicamentoService

--- Geen_Exercicio5/PharmaTech.PharmaAdmin.Domain/Service/MedicamentoService.cs
+++ Geen_Exercicio5/PharmaTech.PharmaAdmin.Domain/Service/MedicamentoService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Geen_Exercicio5.PharmaTech.PharmaAdmin.Domain.Enum;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,25 @@ namespace Geen_Exercicio5.PharmaTech.PharmaAdmin.Domain.Service
 {
     class MedicamentoService
     {
+        public static long CadastrarMedicamento(string nome, double preco, TipoMedicamento tipo)
+        {
+            
+        }
+        public static void AlterarPreco(long codigo, double preco)
+        {
+
+        }
+        public static void ImprimirMedicamento(long codigo)
+        {
+
+        }
+        public static long BuscarCodigoPorNome(string nome)
+        {
+
+        }
+        public static void ImprimirMedicamentoPorTipo(TipoMedicamento tipo)
+        {
+
+        }
     }
 }
