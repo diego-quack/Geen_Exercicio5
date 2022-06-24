f23be8f9a823e789822ff99abb9fe85bda07f42f
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-06-24 17:27:14 -0300

    Testes no program - ImprimirMedicamento e CadastrarMedicamento

--- Geen_Exercicio5/PharmaTech.PharmaAdmin/Program.cs
+++ Geen_Exercicio5/PharmaTech.PharmaAdmin/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Geen_Exercicio5.PharmaTech.PharmaAdmin.Domain.Enum;
+using Geen_Exercicio5.PharmaTech.PharmaAdmin.Domain.Model;
+using Geen_Exercicio5.PharmaTech.PharmaAdmin.Domain.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,29 @@ namespace Geen_Exercicio5
     {
         static void Main(string[] args)
         {
+            List<Medicamento> listaMedicamento = new List<Medicamento>();
+
+            Medicamento paracetamol = new Medicamento
+            {
+                Codigo = 101010,
+                Nome = "Paracetamol",
+                Preco = 4.75,
+                Tipo = TipoMedicamento.Generico
+            };
+
+            Medicamento dipirona = new Medicamento
+            {
+                Codigo = 202020,
+                Nome = "Dipirona",
+                Preco = 6.30,
+                Tipo = TipoMedicamento.Generico
+            };
+
+            Console.WriteLine(MedicamentoService.CadastrarMedicamento(listaMedicamento, paracetamol, paracetamol.Tipo));
+            Console.WriteLine(MedicamentoService.CadastrarMedicamento(listaMedicamento, dipirona, dipirona.Tipo));
+
+            MedicamentoService.ImprimirMedicamento(paracetamol);
+            MedicamentoService.ImprimirMedicamento(dipirona);
         }
     }
 }
