a744b06f2cd4cb151c48f1583c50f58a4c878acb
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-06-24 16:10:51 -0300

    Classe Medicamento

--- Geen_Exercicio5/PharmaTech.PharmaAdmin.Domain/Model/Medicamento.cs
+++ Geen_Exercicio5/PharmaTech.PharmaAdmin.Domain/Model/Medicamento.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Geen_Exercicio5.PharmaTech.PharmaAdmin.Domain.Enum;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,27 @@ namespace Geen_Exercicio5.PharmaTech.PharmaAdmin.Domain.Model
 {
     class Medicamento
     {
+        public long Codigo { get; set; }
+        public string Nome { get; set; }
+        public double  Preco { get; set; }
+        public TipoMedicamento Tipo { get; set; }
+
+        public Medicamento()
+        {
+        }
+
+        public Medicamento(long codigo, string nome, double preco, TipoMedicamento tipo)
+        {
+            Codigo = codigo;
+            Nome = nome;
+            Preco = preco;
+            Tipo = tipo;
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"O medicamento{Nome} de código {Codigo} " +
+                $"é do tipo {Tipo} e custa {Preco.ToString("F2")}");
+        }
     }
 }
